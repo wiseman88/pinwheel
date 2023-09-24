17aa348995547188d50bd8fa4ca3b94415519764
wiseman88 <wiseadam02@gmail.com>
2023-09-24 07:37:25 +0200

    feat: initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="flex h-screen items-center justify-center bg-gradient-to-r from-yellow-100 to-yellow-300 py-2">
+      <h1 className="text-7xl font-extrabold">Pinwheel</h1>
+    </section>
+  );
+};
+
+export default Home;
