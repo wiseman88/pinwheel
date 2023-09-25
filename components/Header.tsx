a0bbdae6c98fa36c8d32afd4604ba977691d1040
wiseman88 <wiseadam02@gmail.com>
2023-09-25 11:34:41 +0200

    fix: svg properties naming

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,14 +40,14 @@ const Header = () => {
             className="-mx-1 h-8 w-8 text-gray-500"
             fill="none"
             stroke="currentColor"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             ></path>
           </svg>
