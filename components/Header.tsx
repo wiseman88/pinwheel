86e6810926a64cee01dc59dd775d88142b909952
wiseman88 <wiseadam02@gmail.com>
2023-10-17 18:42:30 +0200

    feat(header): toggle navbar on mobile
    Add ability to toggle navbar of main header on mobile
    devices by clicking on hamburger menu or close button
    depending on state of isMenuOpen state. Update CSS to
    display ul list with tags under the header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,21 +7,33 @@ import Link from "next/link";
 import navigation from "@/constants/header";
 import logo from "@/public/images/pinwheel.svg";
 import Menu from "./svg/Menu";
+import Close from "./svg/Close";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = usePathname();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="bg-white py-6 lg:bg-transparent">
-      <nav className="container flex flex-row items-center justify-between lg:justify-center">
-        <div>
+      <nav className="container flex flex-row flex-wrap items-center justify-between lg:justify-center">
+        <div className="order-0">
           <Link href="/">
             <Image src={logo} alt="pinwheel" width={147} height={30} />
           </Link>
         </div>
-        <ul className="hidden grow flex-row items-center justify-center lg:flex">
+        <ul
+          className={`order-2 ${
+            isMenuOpen ? "block" : "hidden"
+          } grow basis-full flex-row items-center justify-center text-center lg:flex lg:basis-0`}
+        >
           {navigation.map((item, key) => (
-            <li key={key} className="ml-[20px] first:ml-0">
+            <li key={key} className="first:ml-0 lg:ml-[20px]">
               <Link
                 href={item.path}
                 className={`navlink ${pathname === item.path ? "active" : ""}`}
@@ -30,14 +42,20 @@ const Header = () => {
               </Link>
             </li>
           ))}
+          <Link
+            href="/sing-in"
+            className="btn btn-sm btn-white mt-3.5 block lg:hidden"
+          >
+            Sign In
+          </Link>
         </ul>
-        <div className="hidden lg:block">
+        <div className="order-2 hidden lg:block">
           <Link href="/sing-in" className="btn btn-sm btn-white">
             Sign In
           </Link>
         </div>
-        <button className="block lg:hidden">
-          <Menu />
+        <button className="order-1 block lg:hidden" onClick={toggleMenu}>
+          {isMenuOpen ? <Close /> : <Menu />}
         </button>
       </nav>
     </header>
